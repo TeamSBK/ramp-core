a82c603fabf4d93a1a859e6599cd3ff9f86e5a6b
Harvey Ico <godstrikerharvey@gmail.com>
2013-09-17 18:28:14 +0800

    Bind event models

--- public/assets/js/dnd/drag_and_drop.js
+++ public/assets/js/dnd/drag_and_drop.js
@@ -13,7 +13,7 @@ var createDiagram = function(admin){
         isSource:true,
         scope:"red dot",
         connectorStyle:{ strokeStyle:sourceColor, lineWidth:2 },
-        connector: ["Bezier", { curviness:20 } ],
+        connector: ["Bezier", { curviness:10 } ],
         maxConnections:3,
     };
 
@@ -24,7 +24,8 @@ var createDiagram = function(admin){
         paintStyle:{ fillStyle:targetColor},
         scope:"red dot",
         connectorStyle:{ strokeStyle:targetColor, lineWidth:2 },
-        connector: ["Bezier", { curviness:20 } ],
+        connector: ["Bezier", { curviness:10 } ],
+        connectorOverlays: [['Arrow', {width: 8, length: 15}]],
         maxConnections:3,
         isTarget:true,
         dropOptions : targetDropOptions
@@ -41,6 +42,12 @@ var createDiagram = function(admin){
     bindEventItems();
 
     /* appending new box */
+    jsPlumb.bind('click', function(conn) {
+        jsPlumb.detach({target: conn.targetId, source: conn.sourceId});
+        admin.removeRelationship(conn.sourceId, conn.targetId);
+        console.log('Successfully remove relationship');
+    });
+
     function bindEvent(model) {
         jsPlumb.addEndpoint(model, {anchor: "TopCenter"}, targetEndpoint);
         jsPlumb.addEndpoint(model, {anchor: "BottomCenter"}, sourceEndpoint);
@@ -48,10 +55,28 @@ var createDiagram = function(admin){
     };
 
     function appendNewModel(model) {
-        _modelContainer = $("<div class = 'model-box rounded-corners model-container'>"  +
+        _modelContainer = $("<div class = 'model-box rounded-corners model-container' id = '" + model.modelName + "'>"  +
                         "<div class='model-box-header rounded-corners'>" + model.modelName + "</div></div>");
         _modelContainer.appendTo('#model-container');
         bindEvent(_modelContainer);
+    };
+
+    /* adding relationship */
+
+    function addRelationship(model) {
+        relationship = model.type;
+        var from = jsPlumb.selectEndpoints({source: model.owner}).get(0);
+        var to = jsPlumb.selectEndpoints({ target: model.withModel }).get(0);
+        var attributes = []
+        if(relationship === 'has_many'){
+            var attributes = [['Arrow', { width: 20, length: 30, direction: -1, location: 0.8}]];
+        }
+        else if (relationship === 'belongs_to') {
+            var attributes = [['Diamond', { width: 20, length: 20, direction: -1, location: 0.8}]];
+        }
+
+        jsPlumb.connect({ source: from, target: to, overlays: attributes });
+
     };
 
     var ModelAdminEvents = require ('./lib/ModelAdminEvents.js');
@@ -60,7 +85,13 @@ var createDiagram = function(admin){
         appendNewModel(model);
     });
 
+    admin.on(ModelAdminEvents.RELATIONSHIP_ADDED, function(relationshipObj){
+        addRelationship(relationshipObj);
+    });
 
+    admin.on(ModelAdminEvents.RELATIONSHIP_REMOVED, function(relationshipObj){
+        jsPlumb.detach({ source: relationshipObj.owner, target: relationshipObj.withModel });
+    });
 };
 
 
