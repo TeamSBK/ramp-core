35f3c596f84d9f82db3eec9c07415329435f2698
Nico Bantatua <nbantatua@gmail.com>
2013-09-18 12:04:19 +0800

    added saving, duh.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var Firebase = require('firebase');
-var app = express();
-var port = 3700;
+var ModelAdmin = require("model-admin").ModelAdmin;
+var ModelAdminEvents = require("model-admin").ModelAdminEvents;
+var ServerAdminEvents = require("model-admin").ServerAdminEvents;
 
+var app = express();
+var port = 8002;
 //Set view
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
@@ -16,21 +19,83 @@ var io = require('socket.io').listen(app.listen(port));
 var fieldTypes = new Firebase('https://ramp-model.firebaseio.com/field-types');
 var modelRels = new Firebase('https://ramp-model.firebaseio.com/relationships');
 
+
+var initSockets = function () {
+    io.sockets.on('connection', function (socket) {
+        socket.emit(ServerAdminEvents.SOCKET_CONNECTED, socket.id);
+
+        socket.on(ModelAdminEvents.MODEL_CREATED, function (history) {
+            socket.broadcast.emit(ServerAdminEvents.MODEL_CREATED, history);
+        });
+
+        socket.on(ModelAdminEvents.MODEL_DELETED, function (history) {
+            socket.broadcast.emit(ServerAdminEvents.MODEL_DELETED, history);
+        });
+
+        socket.on(ModelAdminEvents.ATTRIBUTE_ADDED, function (history) {
+            socket.broadcast.emit(ServerAdminEvents.ATTRIBUTE_ADDED, history);
+        });
+
+        socket.on(ModelAdminEvents.ATTRIBUTE_REMOVED, function (history) {
+            socket.broadcast.emit(ServerAdminEvents.ATTRIBUTE_REMOVED, history);
+        });
+
+        socket.on(ModelAdminEvents.RELATIONSHIP_ADDED, function (history) {
+            socket.broadcast.emit(ServerAdminEvents.RELATIONSHIP_ADDED, history);
+        });
+
+        socket.on(ModelAdminEvents.RELATIONSHIP_REMOVED, function (history) {
+            socket.broadcast.emit(ServerAdminEvents.RELATIONSHIP_REMOVED, history);
+        });
+    });
+};
+
+initSockets ();
+
 app.get("/", function(req, res){
     res.render("page");
 });
 
+app.get("/get_app/:id", function(req, res) {
+    var modelAdmin = new Firebase('https://ramp-model.firebaseio.com/model-admin');
+    modelAdmin.on('value', function(snapshot) {
 
-app.get("/:room", function(req, res){
-    res.render("weto");
-});
-
-io.sockets.on('connection', function (socket) {
-    socket.on('send', function (data) {
-        socket.emit('message', { message: 'welcome to the chat' });
-        io.sockets.emit('message', data);
+        var result = findById(snapshot.val(), req.params.id);
+        if (result instanceof Object) {
+            res.send(JSON.stringify(result));
+        } else {
+            res.status(404).send(result);
+        }
     });
 });
 
+app.post("/save", function(req, res) {
+    req.content = "";
+    req.addListener("data", function(chunk) {
+        req.content += chunk;
+    });
+     
+    req.addListener("end", function() {
+        var modelAdmin = new Firebase('https://ramp-model.firebaseio.com/model-admin');
+
+        if (modelAdmin.push(JSON.parse(req.content))) {
+            console.log('[Firebase]Saving done!');
+            res.status(200).send("App models saved!");
+        } else {
+            console.log('[Firebase]Saving failed!');
+            res.status(404).send("App models not saved!");
+        }
+        res.end();
+    });
+});
+
+function findById(source, id) {
+  for (var i = 0; i < source.length; i++) {
+    if (source[i].id == id) {
+      return source[i];
+    }
+  }
+  return "Couldn't find damn app with id: " + id;
+}
 
 console.log("Listening on port " + port);
